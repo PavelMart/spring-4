c762e2f8958a219838e8c91d51af306336fd9631
PavelMart <martackovp@yandex.ru>
2024-04-09 21:13:57 +0300

    Implemented Spring-context with JavaConfig

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,26 +1,27 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
 import ru.netology.router.Router;
 import ru.netology.service.PostService;
-
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
-    private PostController controller;
     private Router router;
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+        final var controller = context.getBean(PostController.class);
+        final var service = context.getBean(PostService.class);
+        final var postRepository = context.getBean(PostRepository.class);
+
         router = new Router(controller);
     }
 
